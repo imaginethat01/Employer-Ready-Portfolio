adbe5867707c014004e10e8a630a72aa45a8fbbb
Anthony J Manenti <33436872+imaginethat01@users.noreply.github.com>
2019-01-14 22:45:26 -0500

    further work minimizing code. Added jumbotron animations. Removed sloppy or unneeded classnames

--- js/scripts.js
+++ js/scripts.js
@@ -45,4 +45,19 @@ for (i = 0; i < dropdown.length; i++) {
 }
 
         
+(function($){
+    // variables
+    elementWidth = $('ul').width(),
+    containerWidth = $('nav').width(),
+    difference = elementWidth-containerWidth,
+    finalWidth = difference * 1.5,
+    element = $('ul');
+    
+    // active on click
+    $('li').on('click', function(){
+      $('li').removeClass('active');
+      $(this).addClass('active');
+    });
+    
+  })(jQuery);
 
